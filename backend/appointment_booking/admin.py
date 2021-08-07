baa4c086b6d38e52fa844be13c733a0982a03912
dhruva-zymr <dhruvad.d@zymr.com>
2021-08-07 15:56:54 +0530

    book appointment changes

--- backend/appointment_booking/admin.py
+++ backend/appointment_booking/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.db import models
+from django.db.models.signals import pre_delete
+from django.dispatch.dispatcher import receiver
 
 from .models import Appointment, Patient
 
@@ -7,6 +10,15 @@ class AppointmentAdmin(admin.ModelAdmin):
     
     list_display = ('start_time', 'end_time', 'patient_pk')
     exclude = [('id')]
+    # actions = ['delete_model']
+
+    # def delete_model(self, request, queryset):
+    #     print("------delete-------queryset-",queryset)
+    #     for obj in queryset:
+    #         obj.patient_pk = None
+    #         obj.delete()
     
+
+
 admin.site.register(Appointment, AppointmentAdmin)
 admin.site.register(Patient)
--- backend/appointment_booking/models.py
+++ backend/appointment_booking/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.db import models
+from django.db.models.signals import pre_delete
+from django.dispatch.dispatcher import receiver
+from django.db.models import ProtectedError
 
 # https://docs.djangoproject.com/en/3.2/topics/db/models/
 
@@ -7,8 +11,28 @@ class Patient(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
 
+# @receiver(pre_delete, sender=Patient)
+# def model_delete(sender, instance, **kwargs):
+#     print("------delete-------sender-",sender, "---instance---",instance,"--kwargs",kwargs)
+#     queryset = Appointment.objects.filter(patient_pk=instance.pk)
+#     print("----apointment--------",queryset)
+    
+#     for obj in queryset:
+#         obj.patient_pk = None
+        # obj.delete()
+        
 
 class Appointment(models.Model):
     start_time = models.DateTimeField(auto_now=False)
     end_time = models.DateTimeField(auto_now=False)
-    patient_pk = models.ForeignKey(Patient, on_delete=models.CASCADE, default=False, blank=True, null=True)
+    patient_pk = models.ForeignKey(Patient, on_delete=models.SET_NULL, default=False, blank=True, null=True)
+
+
+@receiver(pre_delete, sender=Appointment)
+def model_delete(sender, instance, **kwargs):
+    print("------delete-------sender-",sender, "---instance---",instance,"--kwargs",kwargs)
+    print("------instance.patient_pk--------",instance.patient_pk)
+    if instance.patient_pk:
+        print("-------ig--------")
+        raise Exception('Appointments with patients can not be deleted.')
+    
